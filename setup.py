920fa5c12e1415b1889df9667b186b23e05f569e
zuoyi <jianzuoyi@gmail.com>
2016-01-27 02:06:23 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="easytrader",
+    version="0.0.1",
+    packages=find_packages(),
+
+    description="easytrader",
+    auther="EVAN",
+    author_email="529429058@qq.com",
+
+    license='GPL',
+)
