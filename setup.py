82a8276751366d96daf7dd8e0b20a923e272b1b9
zuoyi <jianzuoyi@gmail.com>
2016-01-28 05:07:59 +0800

    add setup.py for install

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="easytrader",
-    version="0.0.1",
+    version="0.1",
     packages=find_packages(),
+    package_data={
+        # if any package contains *.json or *.jar files, include them:
+        '': ['*.json', '*.jar'],
+    },
 
-    description="easytrader",
-    auther="EVAN",
-    author_email="529429058@qq.com",
-
+    # metadata for upload to PyPI
+    auther="me",
+    auther_email="me@example.com",
     license='GPL',
+    description="easytrader is a webtrading program for chinese stocks",
+    keywords="China stock trading",
+    url="http://me.org",
 )
